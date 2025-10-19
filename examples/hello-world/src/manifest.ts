e6543482479a9125f430133b17cf215eecb88340
Sleeyax <yourd3veloper@gmail.com>
2025-10-19 18:11:42 +0200

    feat(examples/hello-world): simplify addon manifest

--- examples/hello-world/src/manifest.ts
+++ examples/hello-world/src/manifest.ts
@@ -1,31 +1,18 @@
 import { type ManifestSchema } from "@stremio-addon/validation-zod";
 
 export const manifest: ManifestSchema = {
-  id: "org.stremio.helloworld",
+  id: "community.stremio.helloworld",
   version: "1.0.0",
-
   name: "Hello World Addon",
   description: "Sample addon providing a few public domain movies",
-
-  // set what type of resources we will return
   resources: ["catalog", "stream"],
-
-  types: ["movie", "series"], // your add-on will be preferred for those content types
-
-  // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+  types: ["movie", "series"],
   catalogs: [
     {
       type: "movie",
-      name: "Hello World Movies",
+      name: "Hello World",
       id: "helloworldmovies",
     },
-    {
-      type: "series",
-      name: "Hello World Series",
-      id: "helloworldseries",
-    },
   ],
-
-  // prefix of item IDs (ie: "tt0032138")
   idPrefixes: ["tt"],
 };

07b65d0a381abf8a593351ecd99c161e03122c8f
Sleeyax <yourd3veloper@gmail.com>
2025-10-19 18:06:17 +0200

    fix(runtime/node-express): fix resource route handling
    Since express v5, the optional character `?` is no longer supported and neither are regular expression like `(foo|bar)`.

--- packages/runtime/node-express/src/router.ts
+++ packages/runtime/node-express/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import type { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import type {
   AddonInterface,
@@ -36,94 +37,109 @@ export function getRouter({ manifest, get }: AddonInterface): Router {
   });
 
   // Extract resource handlers from manifest.
-  const handlersInManifest = [];
-  if (manifest.catalogs.length > 0) handlersInManifest.push("catalog");
+  const handlersInManifest = new Set<string>();
+  if (manifest.catalogs.length > 0) handlersInManifest.add("catalog");
   manifest.resources.forEach((r) =>
-    handlersInManifest.push(typeof r === "string" ? r : r.name),
+    handlersInManifest.add(typeof r === "string" ? r : r.name),
   );
 
-  // Convert the resources array to a regular expression.
-  // This will be used to match the resource in the express route.
-  const ResourcesRegex =
-    handlersInManifest && handlersInManifest.length
-      ? "(" + handlersInManifest.join("|") + ")"
-      : "";
+  // Custom validation for the resource parameter.
+  router.param("resource", (_, res, next, resource) => {
+    if (handlersInManifest.has(resource)) {
+      next();
+    } else {
+      res.writeHead(404);
+      res.end(JSON.stringify({ err: "resource not found" }));
+    }
+  });
 
-  // Handle all resources
-  router.get(
-    `${configPrefix}/:resource${ResourcesRegex}/:type/:id/:extra?.json`,
-    (req, res, next) => {
-      const { resource, type, id } = req.params;
-      const configString =
-        "config" in req.params ? req.params.config : undefined;
-      let config: DefaultConfig = {};
-      // we get `extra` from `req.url` because `req.params.extra` decodes the characters
-      // and breaks dividing querystring parameters with `&`, in case `&` is one of the
-      // encoded characters of a parameter value
-      const extra = req.params.extra
-        ? Object.fromEntries(
-            new URLSearchParams((req.url.split("/").pop() ?? "").slice(0, -5)),
-          )
-        : {};
-      if (configString && (configString || "").length) {
-        try {
-          config = JSON.parse(configString);
-        } catch (_) {
-          // ignore
-        }
+  // Handler function for resource requests
+  const handleResource = (
+    req: Request<{
+      resource: string;
+      type: string;
+      id: string;
+      config?: string;
+      extra?: string;
+    }>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const { resource, type, id } = req.params;
+    const configString = "config" in req.params ? req.params.config : undefined;
+    let config: DefaultConfig = {};
+    // we get `extra` from `req.url` because `req.params.extra` decodes the characters
+    // and breaks dividing querystring parameters with `&`, in case `&` is one of the
+    // encoded characters of a parameter value
+    const extra = req.params.extra
+      ? Object.fromEntries(
+          new URLSearchParams((req.url.split("/").pop() ?? "").slice(0, -5)),
+        )
+      : {};
+    if (configString && (configString || "").length) {
+      try {
+        config = JSON.parse(configString);
+      } catch (_) {
+        // ignore
       }
-      res.setHeader("Content-Type", "application/json; charset=utf-8");
-      get(
-        resource as ShortManifestResource,
-        type as ContentType,
-        id,
-        extra,
-        config,
-      )
-        .then((resp) => {
-          let cacheHeaders = {
-            cacheMaxAge: "max-age",
-            staleRevalidate: "stale-while-revalidate",
-            staleError: "stale-if-error",
-          };
+    }
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    get(
+      resource as ShortManifestResource,
+      type as ContentType,
+      id,
+      extra,
+      config,
+    )
+      .then((resp) => {
+        let cacheHeaders = {
+          cacheMaxAge: "max-age",
+          staleRevalidate: "stale-while-revalidate",
+          staleError: "stale-if-error",
+        };
 
-          const cacheControl = Object.keys(cacheHeaders)
-            .map((prop) => {
-              const cacheProp = cacheHeaders[prop as keyof typeof cacheHeaders];
-              const cacheValue = resp[prop];
-              if (!Number.isInteger(cacheValue)) return false;
-              return cacheProp + "=" + cacheValue;
-            })
-            .filter((val) => !!val)
-            .join(", ");
+        const cacheControl = Object.keys(cacheHeaders)
+          .map((prop) => {
+            const cacheProp = cacheHeaders[prop as keyof typeof cacheHeaders];
+            const cacheValue = resp[prop];
+            if (!Number.isInteger(cacheValue)) return false;
+            return cacheProp + "=" + cacheValue;
+          })
+          .filter((val) => !!val)
+          .join(", ");
 
-          if (cacheControl)
-            res.setHeader("Cache-Control", `${cacheControl}, public`);
+        if (cacheControl)
+          res.setHeader("Cache-Control", `${cacheControl}, public`);
 
-          if (resp.redirect) {
-            res.redirect(307, resp.redirect);
-            return;
+        if (resp.redirect) {
+          res.redirect(307, resp.redirect);
+          return;
+        }
+
+        res.setHeader("Content-Type", "application/json; charset=utf-8");
+
+        res.end(JSON.stringify(resp));
+      })
+      .catch((err) => {
+        if (err.noHandler) {
+          if (next) next();
+          else {
+            res.writeHead(404);
+            res.end(JSON.stringify({ err: "not found" }));
           }
+        } else {
+          console.error(err);
+          res.writeHead(500);
+          res.end(JSON.stringify({ err: "handler error" }));
+        }
+      });
+  };
 
-          res.setHeader("Content-Type", "application/json; charset=utf-8");
+  // Handle all resources with extra parameter.
+  router.get(`${configPrefix}/:resource/:type/:id/:extra.json`, handleResource);
 
-          res.end(JSON.stringify(resp));
-        })
-        .catch((err) => {
-          if (err.noHandler) {
-            if (next) next();
-            else {
-              res.writeHead(404);
-              res.end(JSON.stringify({ err: "not found" }));
-            }
-          } else {
-            console.error(err);
-            res.writeHead(500);
-            res.end(JSON.stringify({ err: "handler error" }));
-          }
-        });
-    },
-  );
+  // Handle all resources without extra parameter.
+  router.get(`${configPrefix}/:resource/:type/:id.json`, handleResource);
 
   return router;
 }

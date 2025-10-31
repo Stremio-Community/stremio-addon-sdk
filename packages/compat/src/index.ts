3e8bd92620599203a970e6b04f0eb1b37dc3e737
Sleeyax <yourd3veloper@gmail.com>
2025-10-31 16:32:39 +0100

    fix(compat): export landing template module
    This slightly deviates from the official SDK's exports but is needed for some addons.

--- packages/compat/src/index.ts
+++ packages/compat/src/index.ts
@@ -2,3 +2,4 @@ export { AddonBuilder as addonBuilder } from "@stremio-addon/linter";
 export { getRouter } from "@stremio-addon/node-express";
 export { serveHTTP } from "./serve-http.js";
 export { publishToCentral } from "./publish-to-central.js";
+export { landingTemplate } from "./landing-template.js";

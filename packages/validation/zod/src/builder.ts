db95d23aa05aa240bd8e93bcccaf0e93fcc8ef30
Sleeyax <yourd3veloper@gmail.com>
2025-12-24 23:22:28 +0100

    fix(validation): add backwards compatibility for zod 3

--- packages/validation/zod/src/builder.ts
+++ packages/validation/zod/src/builder.ts
@@ -1,5 +1,5 @@
 import { AddonBuilder as BaseAddonBuilder } from "@stremio-addon/sdk";
-import type z from "zod";
+import type z from "zod/v4";
 import { manifestSchema } from "./manifest.js";
 
 export class AddonBuilder extends BaseAddonBuilder {
